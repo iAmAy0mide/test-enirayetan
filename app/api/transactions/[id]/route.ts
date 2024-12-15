5d37ee32d91bed0c940c5c4ae05fc375e85a0940
Ayomide <ayomidekenny952@gmail.com>
2024-12-15 01:39:17 -0800

    endpoints setup

---
+++ app/api/transactions/[id]/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse, NextRequest } from "next/server";
+
+
+export async function PUT() {
+
+}
+
+export async function DELETE() {
+
+}
---
+++ app/api/transactions/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse, NextRequest } from "next/server";
+
+
+export async function GET() {
+
+}
+
+export async function POST() {
+
+}
